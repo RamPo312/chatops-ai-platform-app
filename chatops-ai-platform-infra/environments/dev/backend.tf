720bcbe68e37c5b29ce5b77d202569e1d0ba6bbf
Ubuntu <ubuntu@ip-172-31-10-11.us-east-2.compute.internal>
2026-04-10 08:24:26 +0000

    Removed Terraform infra (moved to separate repo)

--- chatops-ai-platform-infra/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "rampo3-chatops-tf-state"
-    key            = "dev/eks/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "rampo3-chatops-tf-lock"
-    encrypt        = true
-  }
-}
